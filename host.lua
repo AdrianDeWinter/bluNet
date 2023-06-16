ad61dccd23f5427e94a71faff280e951e44b8b80
Martin Blecker <martin.blecker@alumni.fh-aachen.de>
2023-06-16 16:45:39 +0200

    Fixed error in host.lua

--- host.lua
+++ host.lua
@@ -1,5 +1,6 @@
 local v = "0.1.0"
 print ("Using Host version "..v)
+
 --stores information about a client, specifically, its id and which modems it is connected to
 HostClass = {id=nil, route={}}
 function HostClass.__init__(baseClass,id,route)
@@ -10,11 +11,12 @@ function HostClass.__init__(baseClass,id,route)
     return self
 end
 setmetatable(HostClass, {__index=table,__call=HostClass.__init__})
+
 function HostClass:toString()
 	ret = "ID: "..self.id
 	if #self.route>0 then
 		ret = ret.."\nHops:\n"
-		for i,hop in pairs(self.route) do
+		for i,hop in ipairs(self.route) do
 			ret = ret..hop
 			if i < #self.route then
 				ret = ret ..", "
@@ -23,16 +25,17 @@ function HostClass:toString()
 	end
 	return ret
 end
+
 function HostClass.equal(host1, host2)
 	--test that both object have an id property that is not nil
 	local result, retval = pcall(function() return host1.id == host2.id and host1.id ~= nil end)
 	return result and retval
 end
 
---takes in a table of host objects and creates new host objects from them (used to restore mthods after transmission over the network)
+--takes in a table of host objects and creates new host objects from them (used to restore class methods after transmission over the network)
 function HostClass.fromTable(t)
 	retval = {}
-	for _,host in pairs(t) do
+	for _,host in ipairs(t) do
 		hostObj = HostClass(host.id, host.route)
 		table.insert(retval, hostObj)
 	end
