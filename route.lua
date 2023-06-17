91fe9d9347ac6ae8c12e56a1c565d1dfbdb277bf
Martin Blecker <martin.blecker@alumni.fh-aachen.de>
2023-06-17 02:11:53 +0200

    Restored separation between startRouting and route. shell does not support opening lua commands in new tabs

--- route.lua
+++ route.lua
@@ -1,8 +1,6 @@
-local v = "0.2.0"
-print ("Running route Script version "..v)
+local v = "0.1.0"
+print ("Using route Script version "..v)
 enable_multishell = true
-verbosity = 1
-
 if(enable_multishell and multishell) then
 	print("Multishell is supported and enabled")
 	tab = shell.openTab("startRouting")
@@ -13,12 +11,5 @@ else
 	elseif not enable_multishell then
 		print("Multishell is disabled")	
 	end
-
-	require('router')
-	require('modem')
-	require('tableUtils')
-
-	local selfRouter = RouterClass(os.getComputerID())
-	selfRouter.modems = ModemClass.getAllModems(selfRouter)
-	selfRouter:listen()
+	shell.run("startRouting")
 end
