2f152d32e90059ff9331b12710e19446610d9188
Martin Blecker <martin.blecker@alumni.fh-aachen.de>
2023-06-19 00:45:41 +0200

    Updated starup script path and made tab variable local

--- route.lua
+++ route.lua
@@ -4,12 +4,12 @@ print ("Using route Script version "..v)
 -- get project root from global context or use this files location
 PROJECT_ROOT = PROJECT_ROOT or "/"..fs.getDir(debug.getinfo(1).source:sub(2))
 
-local ROUTER_STARTUP_SCRIPT_LOCATION = PROJECT_ROOT.."/startRouting.lua"
+local ROUTER_STARTUP_SCRIPT_LOCATION = PROJECT_ROOT.."/startupScripts/startRouting.lua"
 
 enable_multishell = true
 if(enable_multishell and multishell) then
 	print("Multishell is supported and enabled")
-	tab = shell.openTab(ROUTER_STARTUP_SCRIPT_LOCATION)
+	local tab = shell.openTab(ROUTER_STARTUP_SCRIPT_LOCATION)
 	multishell.setTitle(tab, "Router Script")
 else
 	if not multishell then
