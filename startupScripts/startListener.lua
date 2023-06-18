23ebbf04d0b0e298ed3b9de4e09ec03dc16b06dc
Martin Blecker <martin.blecker@alumni.fh-aachen.de>
2023-06-19 00:46:38 +0200

    Added listener script

---
+++ startupScripts/startListener.lua
@@ -0,0 +1,16 @@
+local v = "0.1.0"
+print ("Using startListener Script version "..v)
+
+verbosity=1
+
+-- get project root from global context or use this files location
+PROJECT_ROOT = PROJECT_ROOT or "/"..fs.getDir(debug.getinfo(1).source:sub(2))
+
+require(PROJECT_ROOT.."/lib/modem")
+
+ModemClass.openAllModems()
+
+while true do
+	local src, msg, prtcl = rednet.receive()
+	print("From: "..src..", via "..prtcl.."\n  "..msg)
+end
