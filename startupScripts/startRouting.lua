0b5d971caea876a3568ca5171ef0bc8a65016730
Martin Blecker <martin.blecker@alumni.fh-aachen.de>
2023-06-19 00:47:24 +0200

    Moved startRouting script to startupScript directory

--- startupScripts/startRouting.lua
+++ startupScripts/startRouting.lua
@@ -3,7 +3,7 @@ print ("Using startRouting Script version "..v)
 
 verbosity=1
 
--- get project root from global contxt or use this files location
+-- get project root from global context or use this files location
 PROJECT_ROOT = PROJECT_ROOT or "/"..fs.getDir(debug.getinfo(1).source:sub(2))
 
 require(PROJECT_ROOT.."/lib/router")
