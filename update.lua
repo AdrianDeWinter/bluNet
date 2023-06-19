812193daaeef47b49d7ae34df8acdcae22fee1c1
Martin Blecker <martin.blecker@alumni.fh-aachen.de>
2023-06-19 03:48:55 +0200

    Rewrote update script

--- update.lua
+++ update.lua
@@ -1,4 +1,7 @@
-mkdir bluNet
-rm /bluNet/*
-cp * /bluNet/
+local INSTALL_DIR = (arg[1] or "").."/bluNet"
+print("Installing into "..INSTALL_DIR)
+if(fs.exists(INSTALL_DIR)) then
+	fs.delete(INSTALL_DIR)
+end
+fs.copy(fs.getDir(debug.getinfo(1).source:sub(2)), INSTALL_DIR)
 print("Update completed")
